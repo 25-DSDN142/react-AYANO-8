eeb4f5a0b3fa7e2eb6e59adb8b199686608af5b7
AYANO-8 <ayanodesu0995@gmail.com>
2025-10-05 14:16:39 +1300

    Update 2Draw_faces.js

--- 2Draw_faces.js
+++ 2Draw_faces.js
@@ -65,63 +65,121 @@ function drawInteraction(faces, hands) {
 
 
 
-let leftnose= face.keypoints[237];
+let leftnose= face.keypoints[237];//鼻の孔
 let rightnose= face.keypoints[457];
-let leftnosee= face. keypoints[206];
+let leftnosee= face. keypoints[206];//唇と鼻の間
 let rightnosee= face. keypoints[426];
-let leftnosesa= face. keypoints[187];
-let rightnosesa= face. keypoints[411];
-let end = face. keypoints[234];
-let end2= face. keypoints[454];
+let leftnosesa= face. keypoints[214];//２０６と平行な位置
+let rightnosesa= face. keypoints[434];
+let end1 = face. keypoints[149];
+let end2= face. keypoints[378];//１っ本目の口の線
+
+let leftnose1= face.keypoints[48];
+let rightnose1= face.keypoints[278];
+let leftnose2= face.keypoints[207];
+let rightnose2= face.keypoints[427];
+let leftnose3= face.keypoints[192];
+let rightnose3= face.keypoints[416];
+let end3= face.keypoints[136];
+let end4= face.keypoints[365];//２本目の口の線
+
+let leftnose4= face.keypoints[135];
+let rightnose4= face.keypoints[364];
+let leftnose5= face.keypoints[140];
+let rightnose5= face.keypoints[369];
+let leftnose6= face.keypoints[202];
+let rightnose6= face.keypoints[422];//３本目の口
 
 
 
 
+let leftEyebrow1= face.keypoints[55];
+let rightEyebrow1= face.keypoints[285];
+let leftEyebrow2= face.keypoints[107];
+let rightEyebrow2= face.keypoints[336];
+let leftEyebrow3= face.keypoints[69];
+let rightEyebrow3= face.keypoints[299];
+let leftEyebrow4= face.keypoints[54];
+let rightEyebrow4= face.keypoints[284];
+let leftEyebrow5= face.keypoints[68];
+let rightEyebrow5= face.keypoints[298];
+let leftEyebrow6= face.keypoints[21];
+let rightEyebrow6= face.keypoints[251];
+let leftEyebrow7= face.keypoints[108];
+let rightEyebrow7= face.keypoints[337];
+let leftEyebrow8= face.keypoints[103];
+let rightEyebrow8= face.keypoints[332];
 
+let leftnoseda= face.keypoints[193];
+let rightnoseda= face.keypoints[417];
+let leftnoseda1= face.keypoints[51];
+let rightnoseda1= face.keypoints[281];
+let leftnoseda2= face.keypoints[220];
+let rightnoseda2= face.keypoints[440];
+let leftnoseda3= face.keypoints[198];
+let rightnoseda3= face.keypoints[420];//鼻
+
+let leftcheek= face.keypoints[234];
+let rightcheek= face.keypoints[454];
+let leftcheek1= face.keypoints[117];
+let rightcheek1= face.keypoints[346];
+let leftcheek2= face.keypoints[118];
+let rightcheek2= face.keypoints[347];
+let leftcheek3= face.keypoints[101];
+let rightcheek3= face.keypoints[330];
+
+let leftcheek4= face.keypoints[205];
+let rightcheek4= face.keypoints[425];
+let leftcheek5= face.keypoints[187];
+let rightcheek5= face.keypoints[411];
+let leftcheek6= face.keypoints[123];
+let rightcheek6= face.keypoints[352];
+let leftcheek7= face.keypoints[50];
+let rightcheek7= face.keypoints[280];
+
+
+
+
+//Start drawing on the face here
+    
 noFill();
 stroke(0);
-strokeWeight(7);
-bezier(leftnose.x, leftnose.y, leftnosee.x, leftnosee.y, leftnosesa.x, leftnosesa.y, end.x, end.y);
-/*let noseTip = face.keypoints[4];
-    let upperLip = face.keypoints[13];
-    let leftCheek = face.keypoints[234];
-    let rightCheek = face.keypoints[454];
+strokeWeight(10);
+bezier(leftnose.x, leftnose.y, leftnosee.x, leftnosee.y, leftnosesa.x, leftnosesa.y, end1.x, end1.y);
+bezier(rightnose.x, rightnose.y, rightnosee.x, rightnosee.y, rightnosesa.x, rightnosesa.y, end2.x, end2.y);
+bezier(leftnose1.x, leftnose1.y, leftnose2.x, leftnose2.y, leftnose3.x, leftnose3.y, end3.x, end3.y);
+bezier(rightnose1.x, rightnose1.y, rightnose2.x, rightnose2.y, rightnose3.x, rightnose3.y, end4.x, end4.y);
 
+bezier(leftEyebrow1.x, leftEyebrow1.y, leftEyebrow2.x, leftEyebrow2.y, leftEyebrow3.x, leftEyebrow3.y, leftEyebrow4.x, leftEyebrow4.y);
+bezier(rightEyebrow1.x, rightEyebrow1.y, rightEyebrow2.x, rightEyebrow2.y, rightEyebrow3.x, rightEyebrow3.y, rightEyebrow4.x, rightEyebrow4.y);
+bezier(leftEyebrow1.x, leftEyebrow1.y, leftEyebrow2.x, leftEyebrow2.y, leftEyebrow5.x, leftEyebrow5.y, leftEyebrow6.x, leftEyebrow6.y);
+bezier(rightEyebrow1.x, rightEyebrow1.y, rightEyebrow2.x, rightEyebrow2.y, rightEyebrow5.x, rightEyebrow5.y, rightEyebrow6.x, rightEyebrow6.y);
+bezier(leftEyebrow1.x, leftEyebrow1.y, leftEyebrow2.x, leftEyebrow2.y, leftEyebrow7.x, leftEyebrow7.y, leftEyebrow8.x, leftEyebrow8.y);
+bezier(rightEyebrow1.x, rightEyebrow1.y, rightEyebrow2.x, rightEyebrow2.y, rightEyebrow7.x, rightEyebrow7.y, rightEyebrow8.x, rightEyebrow8.y);
 
-    
+bezier(leftnose.x, leftnose.y, leftnose4.x, leftnose4.y, leftnose5.x, leftnose5.y, leftnose6.x, leftnose6.y);
+bezier(rightnose.x, rightnose.y, rightnose4.x, rightnose4.y, rightnose5.x, rightnose5.y, rightnose6.x, rightnose6.y);
 
-    noFill();
-    stroke(0);
-    strokeWeight(4);
+bezier(leftnoseda.x, leftnoseda.y, leftnoseda1.x, leftnoseda1.y, leftnoseda2.x, leftnoseda2.y, leftnoseda3.x, leftnoseda3.y);
+bezier(rightnoseda.x, rightnoseda.y, rightnoseda1.x, rightnoseda1.y, rightnoseda2.x, rightnoseda2.y, rightnoseda3.x, rightnoseda3.y);
 
-    beginShape();
-    curveVertex(noseTip.x, noseTip.y);
-    curveVertex(noseTip.x, noseTip.y);
-    curveVertex(leftCheek.x, leftCheek.y - 10);
-    curveVertex(upperLip.x, upperLip.y);
-    curveVertex(rightCheek.x, rightCheek.y - 10);
-    curveVertex(upperLip.x, upperLip.y);
-    endShape();*/
+bezier(leftcheek.x, leftcheek.y, leftcheek1.x, leftcheek1.y, leftcheek2.x, leftcheek2.y, leftcheek3.x, leftcheek3.y);
+bezier(rightcheek.x, rightcheek.y, rightcheek1.x, rightcheek1.y, rightcheek2.x, rightcheek2.y, rightcheek3.x, rightcheek3.y);
 
-   
-
-    
-
+bezier(leftcheek3.x, leftcheek3.y, leftcheek5.x, leftcheek5.y, leftcheek6.x, leftcheek6.y, leftcheek7.x, leftcheek7.y);
+bezier(rightcheek3.x, rightcheek3.y, rightcheek5.x, rightcheek5.y, rightcheek6.x, rightcheek6.y, rightcheek7.x, rightcheek7.y);
 
 
 
 
 
-    /*
-    Start drawing on the face here
-    */
-   
 
-    /*drawPoints(face.leftEye);
-    drawPoints(face.leftEyebrow);
-    drawPoints(face.lips);
-    drawPoints(face.rightEye);
-    drawPoints(face.rightEyebrow);*/
+
+ /*drawPoints(face.leftEye);
+ drawPoints(face.leftEyebrow);
+ drawPoints(face.lips);
+ drawPoints(face.rightEye);
+ drawPoints(face.rightEyebrow);*/
     
 
 
