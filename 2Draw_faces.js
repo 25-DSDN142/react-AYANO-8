24552662e1cd9e14f488219a01f226b9ccc3a0dc
AYANO-8 <ayanodesu0995@gmail.com>
2025-10-17 01:11:12 +1300

    Update 2Draw_faces.js

--- 2Draw_faces.js
+++ 2Draw_faces.js
@@ -22,16 +22,8 @@ function drawInteraction(faces, hands) {
       drawPoints(face)
     }
 
-
-
-
-
-
-
-
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
-    let gesture = detectHandGesture(hand);
 
 
 
@@ -40,7 +32,7 @@ let whatGesture = detectHandGesture(hand)
       cheekColor = color(255,192, 203, 150);
     }
     if (whatGesture == "Open Palm") {
-      cheekColor = color(100,200,255, 150);
+      cheekColor = color(255,0,255, 150);
     }
 if (showKeypoints) {
       drawPoints(hand);
@@ -99,106 +91,70 @@ if (showKeypoints) {
 
 
 
+let imagesa = face.keypoints[9];//hat
 
-
-
-
-
-/*let imagesa = face.keypoints[10];
-
-push();
-    imageMode(CENTER);
-   
-    // 顔に追従させたい場合は x, y を face.keypoints[...] に置き換える
-    image(bgImage, imagesa.x, imagesa.y-60,600, 400);
-    pop();
-*/
-
-
-let imagesa = face.keypoints[9];
-
-// 顔の基準として目の間の距離を取得
 let leftEye = face.leftEye.keypoints[0]; 
 let rightEye = face.rightEye.keypoints[3]; 
 let eyedist = dist(leftEye.x, leftEye.y, rightEye.x, rightEye.y);
 
-// 目の距離に応じて画像サイズを変える
-let imgWidth = map(eyedist, 20, 80, 50, 250);  // 目の距離 20～80 に応じて幅 300～600
-let imgHeight = map(eyedist, 20, 80, 10, 120); // 高さも同じく調整
+let imgWidth = map(eyedist, 20, 80, 50, 250);  
+let imgHeight = map(eyedist, 20, 80, 10, 120); 
 
-let offsetY = -70; // 上にずらす
-
+let offsetY = -70; 
 let currentImg = (currentImage === 0) ? bgImage1 : bgImage2;
 
+let eyeDist = dist(leftEyeCenterX, leftEyeCenterY, rightEyeCenterX, rightEyeCenterY);
+let leftcheek = face. keypoints[50];
+let rightcheek = face. keypoints[280];
+
+let cheekW = map(eyeDist, 40, 200, 40, 140);  
+let cheekH = map(eyeDist, 40, 200, 25, 90);   
+
+//eye
+let lensStrokeW = map(eyeDist, 40, 200, 6, 18);   
+let eyeStrokeW  = map(eyeDist, 40, 200, 4, 12);   
+let lashStrokeW = map(eyeDist, 40, 200, 3, 10);   
+let noseStrokeW = map(eyeDist, 40, 200, 2, 6);  
+
+let r = max(leftEyeWidth, leftEyeHeight) * 2; 
+
+let lashLength = map(eyeDist, 40, 200, 5, 25);
+let lashAnglesLeft = [PI/4, PI/4 + 0.15, PI/4 - 0.15];   
+let lashAnglesRight = [3*PI/4, 3*PI/4 + 0.15, 3*PI/4 - 0.15]; 
+
+let noseRadius = map(eyeDist, 40, 200, 10, 60);
+
+
+//image
 push();
 imageMode(CENTER);
 image(currentImg, imagesa.x, imagesa.y + offsetY, imgWidth, imgHeight);
 pop();
 
 
-
-
-
-
-
-let eyeDist = dist(leftEyeCenterX, leftEyeCenterY, rightEyeCenterX, rightEyeCenterY);
-let leftcheek = face. keypoints[50];
-let rightcheek = face. keypoints[280];
-
-
-let cheekW = map(eyeDist, 40, 200, 40, 140);  // 幅
-let cheekH = map(eyeDist, 40, 200, 25, 90);   // 高さ
-
-//let lashLength = map(eyeDist, 40, 200, 5, 25);
-//let noseRadius = map(eyeDist, 40, 200, 10, 60);
-let lensStrokeW = map(eyeDist, 40, 200, 6, 18);   // メガネのふち
-let eyeStrokeW  = map(eyeDist, 40, 200, 4, 12);   // 目の縁
-let lashStrokeW = map(eyeDist, 40, 200, 3, 10);   // まつげ
-let noseStrokeW = map(eyeDist, 40, 200, 2, 6);    // 鼻の線
-
-// 頬
+//cheeks
 fill(cheekColor);
 noStroke();
 ellipse(leftcheek.x, leftcheek.y, cheekW, cheekH);
 ellipse(rightcheek.x, rightcheek.y, cheekW, cheekH);
 
-/*fill(255, 192, 203,150);
-noStroke();
-ellipse(leftcheek.x, leftcheek.y, 120, 70);
-ellipse(rightcheek.x, rightcheek.y, 120, 70);*/
-
-let r = max(leftEyeWidth, leftEyeHeight) * 2; // レンズの直径
-//let bridgeY = (leftEyeCenterY + rightEyeCenterY) / 2;
-//let leftBridgeX = leftEyeCenterX + r/1.5; 
-//let rightBridgeX = rightEyeCenterX - r/1.5; 
-
-
-//let lashLength = 15; // まつげの長さ
-let lashLength = map(eyeDist, 40, 200, 5, 25);
-let lashAnglesLeft = [PI/4, PI/4 + 0.15, PI/4 - 0.15];   // 左目の角度
-let lashAnglesRight = [3*PI/4, 3*PI/4 + 0.15, 3*PI/4 - 0.15]; // 右目の角度
-
-
+//eye
 noStroke();
 fill(255,255,255,);
 ellipse(leftEyeCenterX, leftEyeCenterY, r, r);
 ellipse(rightEyeCenterX, rightEyeCenterY, r, r);
-
 fill(0);
 ellipse(leftEyeCenterX, leftEyeCenterY, r/2.5, r/2.5);
 ellipse(rightEyeCenterX, rightEyeCenterY, r/2.5,r/2.5);
-//arc(leftEyeCenterX, leftEyeCenterY, r/3, r/3, 0, PI);
-//arc(rightEyeCenterX, rightEyeCenterY, r/3, r/3, 0, PI);
 
 fill(222,184,135); 
-arc(leftEyeCenterX, leftEyeCenterY, r, r, PI, 0);  // 上半分
-arc(rightEyeCenterX, rightEyeCenterY, r, r, PI, 0); // 上半分
+arc(leftEyeCenterX, leftEyeCenterY, r, r, PI, 0);  
+arc(rightEyeCenterX, rightEyeCenterY, r, r, PI, 0); 
 
 noFill();
 stroke(0);
 strokeWeight(lensStrokeW);
-// レンズ
-//line(leftBridgeX, bridgeY, rightBridgeX, bridgeY);
+
 line(leftEyeCenterX + r/1.5, (leftEyeCenterY + rightEyeCenterY)/2,
      rightEyeCenterX - r/1.5, (leftEyeCenterY + rightEyeCenterY)/2);
 
@@ -207,13 +163,8 @@ strokeWeight(eyeStrokeW);
 ellipse(leftEyeCenterX, leftEyeCenterY, r, r);
 ellipse(rightEyeCenterX, rightEyeCenterY, r, r);
 
-
-
-
-
-
-
-stroke(0);//したまつげ
+//underlash
+stroke(0);
 strokeWeight(lashStrokeW);
 
 for (let angle of lashAnglesLeft) {
@@ -232,53 +183,46 @@ for (let angle of lashAnglesRight) {
   line(rx, ry, rx2, ry2);
 }
 
-//let noseRadius = 40;
-let noseRadius = map(eyeDist, 40, 200, 10, 60);
-fill(222,184,135);   // 肌色
+//nose
+fill(222,184,135);   
 noStroke();
 ellipse(noseTipX, noseTipY, noseRadius, noseRadius);
 
-// --- 輪郭を描く ---
 noFill();
 stroke(0);
 strokeWeight(noseStrokeW);
 arc(noseTipX, noseTipY, noseRadius, noseRadius, 0, 3*PI/2);
 
 
+
+//lip
 let lipkamo=face.keypoints[186];
 let lipkamo1=face.keypoints[185];
 let lipkamo2=face.keypoints[17];
 let lipkamo3=face.keypoints[410];
-
-bezier(lipkamo.x, lipkamo.y, lipkamo1.x, lipkamo1.y, lipkamo2.x, lipkamo2.y, lipkamo3.x, lipkamo3.y);
-
-
-
-noFill();
 let lipkamo4=face.keypoints[186];
 let lipkamo5=face.keypoints[216];
 let lipkamo6=face.keypoints[206];
 let lipkamo7=face.keypoints[92];
+
+bezier(lipkamo.x, lipkamo.y, lipkamo1.x, lipkamo1.y, lipkamo2.x, lipkamo2.y, lipkamo3.x, lipkamo3.y);
 bezier(lipkamo4.x, lipkamo4.y, lipkamo5.x, lipkamo5.y, lipkamo6.x, lipkamo6.y, lipkamo7.x, lipkamo7.y);
 
 
+//change the hat color when the thumb and index finger touch
 if (hands.length > 0) {
-  let hand = hands[0]; // 1つ目の手
+  let hand = hands[0]; 
   let thumbTip = hand.keypoints[4];
   let indexTip = hand.keypoints[8];
 
-  
   let pinchDist = dist(thumbTip.x, thumbTip.y, indexTip.x, indexTip.y);
 
   if (pinchDist < 30) {
-    currentImage = 1; // 画像を切り替える
+    currentImage = 1; 
   } else {
     currentImage = 0;
   }
 }
-
-
-
 
  /*drawPoints(face.leftEye);
  drawPoints(face.leftEyebrow);
@@ -308,11 +252,6 @@ if (hands.length > 0) {
   //------------------------------------------------------
   // You can make addtional elements here, but keep the face drawing inside the for loop. 
 }
-
-
-
-
-
 
 
 
